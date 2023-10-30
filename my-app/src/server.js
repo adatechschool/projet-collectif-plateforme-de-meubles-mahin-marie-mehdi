c5c7b6d7e063a646dfe4b7d37f119e295e3d9049
Alexandre Trouille <trouillealexandre@hotmail.fr>
2023-10-30 16:56:05 +0100

    middleware de gestion de session

--- my-app/src/server.js
+++ my-app/src/server.js
@@ -6,12 +6,12 @@ app.use((req, res, next) => {
     console.log(`Request received: ${req.method} ${req.url}`);
     next(); // Move on to the next middleware or route
   });
-  
+
   // Middleware for parsing JSON request bodies
   app.use(express.json());
-  
+
   // Define your routes and API endpoints here
-  
+
   // Error handling middleware
   app.use((err, req, res, next) => {
     console.error(err);
