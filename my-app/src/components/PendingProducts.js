aec31e2bc6491deff36e2c55a4f60277f36b50cf
clementine <cleriv.72@gmail.com>
2023-11-02 15:57:54 +0100

    ajout commentaires des choses restant à faire par rapport à l'admin

--- my-app/src/components/PendingProducts.js
+++ my-app/src/components/PendingProducts.js
@@ -1,3 +1,4 @@
+// Ce composant est appelé dans la page Admin.js
 import React, { useState, useEffect } from "react";
 import { Table } from "react-bootstrap";
 import mockStockData from "./mockStockData";
@@ -9,6 +10,8 @@ function PendingProducts() {
 
   useEffect(() => {
     // Pour l'instant j'utilise mon fichier mockStockData.js qui me permet de simuler l'appel API
+    // TODO : Modifier l'appel API pour obtenir l'affichage des données souhaitées.
+    // La boucle qui permet de créer les lignes est déjà réalisé.
     setStockData(mockStockData);
   }, []);
 
--- my-app/src/components/ValidateButton.js
+++ my-app/src/components/ValidateButton.js
@@ -1,5 +1,7 @@
+// Ce composant est utilisé dans le composant PendingProducts.js
 function ValidateButton() {
     return (
+      //TODO : Le bouton devra permettre d'ajouter le produit en attente dans le stock effectif.
       <button type="button" class="btn btn-light">
         Valider
       </button>
