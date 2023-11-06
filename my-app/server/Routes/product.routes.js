95a6bad5f28089589fc48d4623fb3fb8f16fa66c
clementine <cleriv.72@gmail.com>
2023-11-06 20:36:28 +0100

    modification du code avec utilisation de sqlite

--- my-app/server/Routes/product.routes.js
+++ my-app/server/Routes/product.routes.js
@@ -1,83 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Product = require();// TODO: Import the model using sequelise and mysql2 libraries.
-
 
 // GET all products
-router.get('/', async (req, res) => {
-    try {
-        const products = await Product.find();
-        res.json(products);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
+router.get("/product", (req, res) => {
+  try {
+    const sql = "SELECT * FROM Product ORDER BY name";
+    req.db.all(sql, [], (err, rows) => {
+      if (err) {
+        res.status(404).json({ message: "Cannot find product" });
+      }
+      res.json(rows)
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 // GET a single product by ID
-router.get('/product:id', getProduct, (req, res) => {
-    res.json(res.product);
-});
-
-// CREATE a new product
-router.post('/product:new', async (req, res) => {
-    const product = new Product({
-        name: req.body.name,
-        description: req.body.description,
-        price: req.body.price
-    });
-
+router.get("/product/:id", (req, res) => {
+    const id = req.params.id;
     try {
-        const newProduct = await product.save();
-        res.status(201).json(newProduct);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-});
-
-// UPDATE a product by ID
-router.patch('/product/update:id', getProduct, async (req, res) => {
-    if (req.body.name != null) {
-        res.product.name = req.body.name;
-    }
-    if (req.body.description != null) {
-        res.product.description = req.body.description;
-    }
-    if (req.body.price != null) {
-        res.product.price = req.body.price;
-    }
-
-    try {
-        const updatedProduct = await res.product.save();
-        res.json(updatedProduct);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-});
-
-// DELETE a product by ID
-router.delete('/product/delete:id', getProduct, async (req, res) => {
-    try {
-        await res.product.remove();
-        res.json({ message: 'Product deleted' });
-    } catch (err) {
+        const sql = "SELECT * FROM Product WHERE id = ?";
+        req.db.all(sql, id, (err, rows) => {
+          if (err) {
+            res.status(404).json({ message: "Cannot find product" });
+          }
+          res.json(rows)
+        });
+      } catch (err) {
         res.status(500).json({ message: err.message });
-    }
+      }
 });
 
-// Middleware function to get a product by ID
-async function getProduct(req, res, next) {
-    let product;
-    try {
-        product = await Product.findById(req.params.id);
-        if (product == null) {
-            return res.status(404).json({ message: 'Cannot find product' });
-        }
-    } catch (err) {
-        return res.status(500).json({ message: err.message });
-    }
-
-    res.product = product;
-    next();
-}
 
 module.exports = router;
--- my-app/src/Database/connectionDatabase.js
+++ my-app/src/Database/connectionDatabase.js
@@ -1,12 +1,10 @@
-const mysql = require("mysql");
+const sqlite3 = require('sqlite3').verbose()
+const path = require('path');
 
-
-    const connexionDatabase = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        port: 3306,
-        password: process.env.password,
-        database: 'copameba'
-    })
-
-module.exports = connexionDatabase;
+const db = new sqlite3.Database("copameba.db", err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Connexion réussie à la base de données 'copameba.db'");
+});
+module.exports = db;
