a8c914007b3dd612d282546a87473bf28233b063
MarieCoulay <marie.coulay@gmail.com>
2023-11-07 11:35:35 +0100

    changement path db

--- my-app/server/server.js
+++ my-app/server/server.js
@@ -3,13 +3,9 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 // db est défini dans la page connectionDatabase
-<<<<<<< HEAD
+
 const db = require("../server/database/connectionDatabase");
 const router = require("../server/Routes/product.routes");
-=======
-const db = require("../src/database/connectionDatabase");
-const router = require('../server/Routes/product.routes')
->>>>>>> aa87b621cbb0b80b642085619520ae965980e196
 
 app.use(function (req, res, next) {
   req.db = db;
@@ -21,8 +17,4 @@ app.use(router);
 // Start the server
 app.listen(8080, () => {
   console.log("Server is running");
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> aa87b621cbb0b80b642085619520ae965980e196
