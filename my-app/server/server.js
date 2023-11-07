aa87b621cbb0b80b642085619520ae965980e196
root <root@LAPTOP-I4U4E0I4>
2023-11-07 11:21:05 +0100

    branch cleaning done

--- my-app/server/server.js
+++ my-app/server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 // db est défini dans la page connectionDatabase
-const db = require("../src/Database/connectionDatabase");
+const db = require("../src/database/connectionDatabase");
 const router = require('../server/Routes/product.routes')
 
 app.use(function (req, res, next) {
@@ -18,15 +18,3 @@ app.use(router);
 app.listen(8080, () => {
   console.log("Server is running");
 });
-
-
-
-// connexionDatabase.connect()
-// connexionDatabase.query('SELECT * FROM product_category', [], (erreur, resultat)=>{
-//         if(erreur){
-//           console.log(erreur);
-//         } else {
-//           res.status(200).render("../public/index.js", {resultat});
-//           console.log("Hello world", resultat);
-//         }
-//       })
