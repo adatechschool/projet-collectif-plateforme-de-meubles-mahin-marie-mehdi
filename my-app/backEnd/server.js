e9d43c80347818996755e2050f0c45396954e57e
MarieCoulay <marie.coulay@gmail.com>
2023-11-08 13:14:05 +0100

    cors fixed

--- my-app/backEnd/server.js
+++ my-app/backEnd/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 require("dotenv").config();
@@ -12,7 +13,7 @@ app.use(function (req, res, next) {
   req.db = db;
   next();
 });
-
+app.use(cors());
 app.use(router);
 app.use(catRouter);
 app.use(userRouter);
