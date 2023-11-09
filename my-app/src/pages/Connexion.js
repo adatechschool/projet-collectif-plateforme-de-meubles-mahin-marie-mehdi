3baf7fe15919045f27f9dfeea72b292eedff3ee4
Alexandre Trouille <trouillealexandre@hotmail.fr>
2023-11-09 12:17:00 +0100

    appel api plus test, problème lié à l'email

--- my-app/src/pages/Connexion.js
+++ my-app/src/pages/Connexion.js
@@ -1,3 +1,67 @@
+// import React, { useState, useEffect } from "react";
+// import ConnexionPassword from "../components/ConnexionPassword";
+// import { Link } from "react-router-dom";
+// import Email from "../components/Email";
+// import axios from "axios";
+
+// const Connexion = () => {
+//   const [password, setPassword] = useState("");
+//   const [errorMessage, setErrorMessage] = useState("");
+//   const [email, setEmail] = useState("");
+
+//   // appel api à réaliser
+//   const [stockData, setStockData] = useState([]);
+
+//   const HandleConnexionClick = (email) => {
+
+
+//     {/* Replaced teh mock data with an appel to the API endpoint for products using axios */}
+//     useEffect((email) => {
+//       axios.get(`http://localhost:8080/user/:${email}`)
+//         .then(response => {
+//           setStockData(response.data);
+//           console.log(response.data);
+//         })
+//         .catch(error => {
+//           console.error('There was an error!', error);
+//         });
+//     }, []);
+
+//     if (password === stockData.password) {
+//       // TODO TODO TODO TODO TODO TODO
+//       // Les mots de passe correspondent, effectuez l'inscription ici
+//       // Mettre ici la requête au serveur pour enregistrer les données de l'utilisateur
+//       setErrorMessage("Vous êtes connecté");
+//     } else {
+//       setErrorMessage("Echec de la connexion, veuillez vérifier vos identifiant");
+//     }
+//   }
+
+//   return (
+//     <div className="text-center">
+//       <h1>Connexion</h1>
+//       <Email setEmail={setEmail}/>
+//       <ConnexionPassword setPassword={setPassword} />
+//       <button
+//           className="btn btn-secondary btn-lg"
+//           onClick={HandleConnexionClick}
+//         >
+//           Se connecter
+//         </button>
+//       <p className="text-center">
+//         Vous n'avez pas de compte ?
+//         <Link to="/inscription" className="nav-link" id="signIn">
+//           Inscrivez-vous ici.
+//         </Link>
+//       </p>
+//       errorMessage ? <p>{errorMessage}</p> : null
+//     </div>
+//   );
+// };
+
+// export default Connexion;
+
+
 import React, { useState, useEffect } from "react";
 import ConnexionPassword from "../components/ConnexionPassword";
 import { Link } from "react-router-dom";
@@ -8,16 +72,12 @@ const Connexion = () => {
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [email, setEmail] = useState("");
+  const [stockData, setStockData] = useState(null);
 
-  // appel api à réaliser
-  const [stockData, setStockData] = useState([]);
-
-  const HandleConnexionClick = (email) => {
-
-
-    {/* Replaced teh mock data with an appel to the API endpoint for products using axios */}
-    useEffect((email) => {
-      axios.get(`http://localhost:8080/user/:${email}`)
+  useEffect(() => {
+    // Appel à l'API pour obtenir les données utilisateur
+    if (email) {
+      axios.get(`http://localhost:8080/user/${email}`)
         .then(response => {
           setStockData(response.data);
           console.log(response.data);
@@ -25,29 +85,30 @@ const Connexion = () => {
         .catch(error => {
           console.error('There was an error!', error);
         });
-    }, []);
+    }
+  }, [email]);
 
-    if (password === stockData.password) {
-      // TODO TODO TODO TODO TODO TODO
+  const HandleConnexionClick = () => {
+    if (stockData && password === stockData.password) {
       // Les mots de passe correspondent, effectuez l'inscription ici
       // Mettre ici la requête au serveur pour enregistrer les données de l'utilisateur
       setErrorMessage("Vous êtes connecté");
     } else {
-      setErrorMessage("Echec de la connexion, veuillez vérifier vos identifiant");
+      setErrorMessage("Échec de la connexion, veuillez vérifier vos identifiants");
     }
   }
 
   return (
     <div className="text-center">
       <h1>Connexion</h1>
-      <Email setEmail={setEmail}/>
+      <Email setEmail={setEmail} />
       <ConnexionPassword setPassword={setPassword} />
       <button
-          className="btn btn-secondary btn-lg"
-          onClick={HandleConnexionClick}
-        >
-          Se connecter
-        </button>
+        className="btn btn-secondary btn-lg"
+        onClick={HandleConnexionClick}
+      >
+        Se connecter
+      </button>
       <p className="text-center">
         Vous n'avez pas de compte ?
         <Link to="/inscription" className="nav-link" id="signIn">
@@ -60,3 +121,4 @@ const Connexion = () => {
 };
 
 export default Connexion;
+
