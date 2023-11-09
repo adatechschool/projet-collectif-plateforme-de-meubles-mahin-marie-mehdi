46b6911c58ceddfcaab3dc8d9641c47af2ed615c
root <root@LAPTOP-I4U4E0I4>
2023-11-09 14:41:04 +0100

    post user inscription WIP

--- my-app/src/components/CardsGenerator.js
+++ my-app/src/components/CardsGenerator.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import logo from "../assets/images/COPMEBA.png";
 import { Link } from "react-router-dom";
 
 function CardsGenerator() {
