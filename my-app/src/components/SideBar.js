e74ba117d80d78a83e26f78bc3516ad043b2901b
clementine <cleriv.72@gmail.com>
2023-11-10 12:34:41 +0100

    side barre terminée, à améliorer + tard

--- my-app/src/components/SideBar.js
+++ my-app/src/components/SideBar.js
@@ -41,118 +41,120 @@ function Sidebar() {
             <div className="dropdown-item">
               <Link to={`./category/1`}>Chaises</Link>
             </div>
-            <Link to={`./category/2`}>Tables</Link>
-            <a className="dropdown-item" href="#">
-              Tables basses
-            </a>
-            <a className="dropdown-item" href="#">
-              Canapés
-            </a>
-            <a className="dropdown-item" href="#">
-              Buffets
-            </a>
-            <a className="dropdown-item" href="#">
-              Commodes
-            </a>
-            <a className="dropdown-item" href="#">
-              Etagères
-            </a>
-            <a className="dropdown-item" href="#">
-              Bibliothèques
-            </a>
-            <a className="dropdown-item" href="#">
-              Fauteuils
-            </a>
-            <a className="dropdown-item" href="#">
-              Chaises de bureaux
-            </a>
-            <a className="dropdown-item" href="#">
-              Meubles TV
-            </a>
-            <a className="dropdown-item" href="#">
-              Tabourets de bar
-            </a>
-            <a className="dropdown-item" href="#">
-              Consoles
-            </a>
-            <a className="dropdown-item" href="#">
-              Coiffeuses
-            </a>
-            <a className="dropdown-item" href="#">
-              Armoires
-            </a>
-            <a className="dropdown-item" href="#">
-              Vaisseliers
-            </a>
-            <a className="dropdown-item" href="#">
-              Penderies
-            </a>
-            <a className="dropdown-item" href="#">
-              Tables de chevet
-            </a>
+            <div className="dropdown-item">
+              <Link to={`./category/2`}>Tables</Link>
+            </div>
+            <div className="dropdown-item">
+              <Link to={`./category/3`}>Tables Basses</Link>
+            </div>
+            <div className="dropdown-item">
+              <Link to={`./category/4`}>Canapés</Link>
+            </div>
+            <div className="dropdown-item">
+              <Link to={`./category/5`}>Buffets</Link>
+            </div>
+            <div className="dropdown-item">
+              <Link to={`./category/6`}>Commodes</Link>
+            </div>
+            <div className="dropdown-item">
+              <Link to={`./category/7`}>Etagères</Link>
+            </div>
+            <div className="dropdown-item">
+              <Link to={`./category/8`}>Bibliothèques</Link>
+            </div>
+            <div className="dropdown-item">
+              <Link to={`./category/9`}>Fauteuils</Link>
+            </div>
+            <div className="dropdown-item">
+              <Link to={`./category/10`}>Chaises de Bureau</Link>
+            </div>
+            <div className="dropdown-item">
+              <Link to={`./category/11`}>Meubles TV</Link>
+            </div>
+            <div className="dropdown-item">
+              <Link to={`./category/12`}>Tabourets de bar</Link>
+            </div>
+            <div className="dropdown-item">
+              <Link to={`./category/13`}>Consoles</Link>
+            </div>
+            <div className="dropdown-item">
+              <Link to={`./category/14`}>Coiffeuses</Link>
+            </div>
+            <div className="dropdown-item">
+              <Link to={`./category/15`}>Armoires</Link>
+            </div>
+            <div className="dropdown-item">
+              <Link to={`./category/16`}>Vaisseliers</Link>
+            </div>
+            <div className="dropdown-item">
+              <Link to={`./category/17`}>Penderies</Link>
+            </div>
+            <div className="dropdown-item">
+              <Link to={`./category/18`}>Tables de chevet</Link>
+            </div>
           </div>
-        </div>
-        <div className="dropdown-open">
-          <a
-            className="btn border-none dropdown-toggle text-white"
-            type="button"
-            id="triggerId"
-            data-bs-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            <i className="bi bi-cash-coin"></i>
-            <span className="fs-5 ms-3">Prix</span>
-          </a>
-          <div className="dropdown-menu" aria-labelledby="triggerId">
-            <a className="dropdown-item" href="#">
-              0€ - 100€
+          <div className="dropdown-open">
+            <a
+              className="btn border-none dropdown-toggle text-white"
+              type="button"
+              id="triggerId"
+              data-bs-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <i className="bi bi-cash-coin"></i>
+              <span className="fs-5 ms-3">Prix</span>
             </a>
-            <a className="dropdown-item" href="#">
-              100€ - 200€
-            </a>
+            <div className="dropdown-menu" aria-labelledby="triggerId">
+              <a className="dropdown-item" href="#">
+                0€ - 100€
+              </a>
+              <a className="dropdown-item" href="#">
+                100€ - 200€
+              </a>
+            </div>
           </div>
-        </div>
-        <div className="dropdown-open">
-          <a
-            className="btn border-none dropdown-toggle text-white"
-            type="button"
-            id="triggerId"
-            data-bs-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            <i className="bi bi-gem"></i>
-            <span className="fs-5 ms-3">Matériaux</span>
-          </a>
-          <div className="dropdown-menu" aria-labelledby="triggerId">
-            <a className="dropdown-item" href="#">
-              Bois
+          <div className="dropdown-open">
+            <a
+              className="btn border-none dropdown-toggle text-white"
+              type="button"
+              id="triggerId"
+              data-bs-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <i className="bi bi-gem"></i>
+              <span className="fs-5 ms-3">Matériaux</span>
             </a>
-            <a className="dropdown-item" href="#">
-              Métal
-            </a>
+            <div className="dropdown-menu" aria-labelledby="triggerId">
+              <a className="dropdown-item" href="#">
+                Bois
+              </a>
+              <a className="dropdown-item" href="#">
+                Métal
+              </a>
+            </div>
           </div>
-        </div>
-        <div className="dropdown-open">
-          <a
-            className="btn border-none dropdown-toggle text-white"
-            type="button"
-            id="triggerId"
-            data-bs-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            <i className="bi bi-palette"></i>
-            <span className="fs-5 ms-3">Couleurs</span>
-          </a>
-          <div className="dropdown-menu" aria-labelledby="triggerId">
-            <a className="dropdown-item" href="#">
-              Noir
+          <div className="dropdown-open">
+            <a
+              className="btn border-none dropdown-toggle text-white"
+              type="button"
+              id="triggerId"
+              data-bs-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <i className="bi bi-palette"></i>
+              <span className="fs-5 ms-3">Couleurs</span>
             </a>
-            <a className="dropdown-item" href="#">
-              Blanc
-            </a>
+            <div className="dropdown-menu" aria-labelledby="triggerId">
+              <a className="dropdown-item" href="#">
+                Noir
+              </a>
+              <a className="dropdown-item" href="#">
+                Blanc
+              </a>
+            </div>
           </div>
         </div>
       </div>
